2f3d45a081da249778038fb26c761ec9c4c94828
rivaldom@afrosoft.co.zw <rivaldom@afrosoft.co.zw>
2022-04-06 11:16:30 +0200

    set up done

---
+++ lib/pages/home.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomepageUi();
+  }
+}
+
+class _HomepageUi extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
